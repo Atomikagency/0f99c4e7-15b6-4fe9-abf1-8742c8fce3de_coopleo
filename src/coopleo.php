48173467525c10f214aedbc1b045771047f19fee
Kevin Janiky <kevin.janiky@gmail.com>
2025-03-31 12:29:20 +0200

    base PHP

---
+++ src/coopleo.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Plugin Name: Coopleo
+ * Description: Add powerfull search engine on website
+ * Version: 1.0.0
+ * Author: AtomikAgency
+ * Author URI: https://atomikagency.fr/
+ */
+
+define('COOPLEO_PLUGIN_DIR', plugin_dir_path(__FILE__));
+define('COOPLEO_PLUGIN_URL', plugin_dir_url(__FILE__));
+
+if (!defined('ABSPATH')) {
+    exit; // Prevent direct access
+}
+
+require_once COOPLEO_PLUGIN_DIR . 'update-checker.php';
