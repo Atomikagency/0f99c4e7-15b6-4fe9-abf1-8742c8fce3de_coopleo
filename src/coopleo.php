ac7ecbf9ef8e501349750f76b48af1ae65219453
Kevin JANIKY <kevin.janiky@gmail.com>
2025-04-08 16:22:21 +0200

    Update API endpoint for search functionality

--- src/coopleo.php
+++ src/coopleo.php
@@ -11,7 +11,7 @@
 define('COOPLEO_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('COOPLEO_PLUGIN_URL', plugin_dir_url(__FILE__));
 define('COOPLEO_SEARCH_PAGE_RESULT', 57114);
-define('COOPLEO_API_ENDPOINT','https://coopleo_backend.test/api/search');
+define('COOPLEO_API_ENDPOINT','https://coopleo.api.demo.atomikagency.fr/api/search');
 
 if (!defined('ABSPATH')) {
     exit; // Prevent direct access
