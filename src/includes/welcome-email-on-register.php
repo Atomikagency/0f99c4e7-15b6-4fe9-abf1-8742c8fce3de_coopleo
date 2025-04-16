867e7ddff04f17a150f11ebaea5e08d4af044c5e
Kevin JANIKY <kevin.janiky@gmail.com>
2025-04-16 17:58:57 +0200

    Add welcome email functionality for new user registrations

--- src/includes/welcome-email-on-register.php
+++ src/includes/welcome-email-on-register.php
@@ -5,10 +5,27 @@ add_action('user_register', 'envoyer_mail_si_pas_amelia_employee', 10, 1);
 
 function envoyer_mail_si_pas_amelia_employee($user_id) {
     $user = get_userdata($user_id);
-    $roles = $user->roles;
-    $to = 'kevin.janiky@gmail.com';
-    $subject = 'Un nouvel utilisateur sans le rôle Amelia Employee';
-    $message = 'Un nouvel utilisateur a été créé : ' . $user->user_login . ' (' . $user->user_email . ')' . json_encode($roles).' ';
-    $headers = ['Content-Type: text/html; charset=UTF-8'];
-        wp_mail($to, $subject, $message, $headers);
- }
+    $webhook_url = 'https://automate.kevinjaniky.fr/webhook/aef91aca-efce-4989-a63c-90bbdd493461';
+    $reset_key = get_password_reset_key($user);
+    $user_login = $user->user_login;
+    $site_url = site_url();
+    $reset_url = $site_url . '/wp-login.php?action=rp&key=' . $reset_key . '&login=' . rawurlencode($user_login);
+    $data = array(
+        'user_id' => $user_id,
+        'password_reset_url' => $reset_url
+    );
+    $ch = curl_init($webhook_url);
+
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+    $response = curl_exec($ch);
+    if (curl_errno($ch)) {
+        error_log('Erreur cURL : ' . curl_error($ch));
+    }
+
+    curl_close($ch);
+}
