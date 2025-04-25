c76f815bf1511d186e1a8efc63bc94f77cd132bb
Kevin JANIKY <kevin.janiky@gmail.com>
2025-04-25 09:11:46 +0200

    Change webhook

--- src/includes/welcome-email-on-register.php
+++ src/includes/welcome-email-on-register.php
@@ -5,7 +5,7 @@ add_action('user_register', 'envoyer_mail_si_pas_amelia_employee', 10, 1);
 
 function envoyer_mail_si_pas_amelia_employee($user_id) {
     $user = get_userdata($user_id);
-    $webhook_url = 'https://automate.kevinjaniky.fr/webhook/aef91aca-efce-4989-a63c-90bbdd493461';
+    $webhook_url = 'https://hook.eu2.make.com/lc3x65m5jmf48u2ggjcbkpwztu1rsub7';
     $reset_key = get_password_reset_key($user);
     $user_login = $user->user_login;
     $site_url = site_url();
