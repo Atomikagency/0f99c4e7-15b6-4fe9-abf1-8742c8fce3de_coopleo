f04e63e539be1e825ce578e206f226d380c845fa
Clement Menczynski <71343161+clementmenc@users.noreply.github.com>
2025-05-15 15:25:47 +0200

    Add simple card in result

--- src/templates/results.tpl.php
+++ src/templates/results.tpl.php
@@ -102,66 +102,9 @@
                             <img src="<?php echo COOPLEO_PLUGIN_URL . 'assets/icons/visio-en-visio-rond.png'; ?>" alt="">
                             <p class="tooltip">En visio</p>
                         </div>
-                        <!-- <svg class="rdv-type-cabinet" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="30" height="30" viewBox="0 0 30 30">
-                            <defs>
-                                <clipPath id="clip-path">
-                                <rect id="Rectangle_808" data-name="Rectangle 808" width="14.146" height="14.139" transform="translate(0 0)" fill="#c92c61"/>
-                                </clipPath>
-                            </defs>
-                            <g id="Groupe_715" data-name="Groupe 715" transform="translate(-635 -633)">
-                                <g id="Ellipse_7" data-name="Ellipse 7" transform="translate(635 633)" fill="none" stroke="#c92c62" stroke-width="1" opacity="0.997">
-                                <circle cx="15" cy="15" r="15" stroke="none"/>
-                                <circle cx="15" cy="15" r="14.5" fill="none"/>
-                                </g>
-                                <g id="Groupe_505" data-name="Groupe 505" transform="translate(643 640.93)">
-                                <g id="Groupe_500" data-name="Groupe 500" transform="translate(0 0)" clip-path="url(#clip-path)">
-                                    <path id="Tracé_264" data-name="Tracé 264" d="M12.86,7.591V9.645a4.5,4.5,0,0,1-9,0v-.7A4.5,4.5,0,0,1,0,4.495V.64A.635.635,0,0,1,.64,0H2.575a.635.635,0,0,1,.64.64.635.635,0,0,1-.64.64H1.295V4.495a3.215,3.215,0,1,0,6.43,0V1.28H6.445A.635.635,0,0,1,5.8.64.635.635,0,0,1,6.445,0H8.38A.642.642,0,0,1,9,.64V4.495A4.514,4.514,0,0,1,5.15,8.945v.7a3.215,3.215,0,1,0,6.43,0V7.591a1.925,1.925,0,1,1,2.456-1.176A1.955,1.955,0,0,1,12.86,7.591" transform="translate(0 0)" fill="#c92c61"/>
-                                </g>
-                                </g>
-                            </g>
-                        </svg>
-                        <svg class="rdv-type-visio" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="30" height="30" viewBox="0 0 30 30">
-                            <defs>
-                                <clipPath id="clip-path">
-                                <rect id="Rectangle_806" data-name="Rectangle 806" width="14.084" height="14.082" transform="translate(0 0)" fill="#c92c61"/>
-                                </clipPath>
-                            </defs>
-                            <g id="Groupe_717" data-name="Groupe 717" transform="translate(-703 -633)">
-                                <g id="Ellipse_9" data-name="Ellipse 9" transform="translate(703 633)" fill="none" stroke="#c92c62" stroke-width="1" opacity="0.997">
-                                <circle cx="15" cy="15" r="15" stroke="none"/>
-                                <circle cx="15" cy="15" r="14.5" fill="none"/>
-                                </g>
-                                <g id="Groupe_507" data-name="Groupe 507" transform="translate(711.063 639.729)">
-                                <g id="Groupe_498" data-name="Groupe 498" transform="translate(0 0)" clip-path="url(#clip-path)">
-                                    <path id="Tracé_260" data-name="Tracé 260" d="M13.775,25.937a.7.7,0,0,0-.656-.07L10.53,26.9a2.113,2.113,0,0,0-2.08-1.79H2.113A2.113,2.113,0,0,0,0,27.225v4.929a2.113,2.113,0,0,0,2.113,2.113H8.45a2.113,2.113,0,0,0,2.08-1.789l2.588,1.035a.7.7,0,0,0,.965-.654V26.52A.7.7,0,0,0,13.775,25.937ZM8.45,32.858H2.113a.7.7,0,0,1-.7-.7V27.225a.7.7,0,0,1,.7-.7H8.45a.7.7,0,0,1,.7.7v4.929a.7.7,0,0,1-.7.7m4.225-1.04-2.113-.845V28.405l2.113-.845v4.258Z" transform="translate(0 -20.184)" fill="#c92c61"/>
-                                    <path id="Tracé_261" data-name="Tracé 261" d="M10.268,2.353a3.319,3.319,0,0,1,4.648,0,.7.7,0,0,0,.986-1,4.733,4.733,0,0,0-6.619,0,.7.7,0,0,0,.986,1Z" transform="translate(-7.311 0)" fill="#c92c61"/>
-                                    <path id="Tracé_262" data-name="Tracé 262" d="M17.7,12.672a.7.7,0,1,0,.937,1.051c.017-.015.033-.031.049-.048a.9.9,0,0,1,1.268,0,.7.7,0,1,0,1.034-.956c-.016-.017-.032-.033-.049-.048A2.317,2.317,0,0,0,17.7,12.672Z" transform="translate(-14.034 -9.654)" fill="#c92c61"/>
-                                </g>
-                                </g>
-                            </g>
-                        </svg> -->
                    </div>
                    <span class="coopleo-button" >
                         <img width="16" height="16" src="<?php echo COOPLEO_PLUGIN_URL . 'assets/icons/calendrier.png'; ?>" alt="">
-                        <!-- <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" id="Groupe_516" data-name="Groupe 516" width="15.158" height="15.982" viewBox="0 0 15.158 15.982">
-                            <defs>
-                                <clipPath id="clip-path">
-                                <rect id="Rectangle_817" data-name="Rectangle 817" width="15.158" height="15.982" fill="#fff"/>
-                                </clipPath>
-                            </defs>
-                            <g id="Groupe_515" data-name="Groupe 515" clip-path="url(#clip-path)">
-                                <path id="Tracé_268" data-name="Tracé 268" d="M107.25,3.345a.575.575,0,0,1-.583-.558V.558a.584.584,0,0,1,1.166,0v2.23a.575.575,0,0,1-.583.558" transform="translate(-102.78)" fill="#fff"/>
-                                <path id="Tracé_269" data-name="Tracé 269" d="M277.916,3.345a.575.575,0,0,1-.583-.558V.558a.584.584,0,0,1,1.166,0v2.23a.575.575,0,0,1-.583.558" transform="translate(-267.228)" fill="#fff"/>
-                                <path id="Tracé_270" data-name="Tracé 270" d="M112.777,241.6a.869.869,0,0,1-.552-.216.751.751,0,0,1-.225-.528.713.713,0,0,1,.062-.283.854.854,0,0,1,.163-.245.808.808,0,0,1,.257-.156.821.821,0,0,1,.847.156.764.764,0,0,1,.225.528,1.072,1.072,0,0,1-.016.149.459.459,0,0,1-.047.134.555.555,0,0,1-.07.134,1.178,1.178,0,0,1-.093.112A.837.837,0,0,1,112.777,241.6Z" transform="translate(-107.919 -231.754)" fill="#fff"/>
-                                <path id="Tracé_271" data-name="Tracé 271" d="M187.444,241.508a.869.869,0,0,1-.552-.216.751.751,0,0,1-.225-.528.713.713,0,0,1,.062-.282.854.854,0,0,1,.163-.245.829.829,0,0,1,1.1,0,.764.764,0,0,1,.225.528,1.07,1.07,0,0,1-.016.149.459.459,0,0,1-.047.134.554.554,0,0,1-.07.134,1.179,1.179,0,0,1-.093.112A.837.837,0,0,1,187.444,241.508Z" transform="translate(-179.865 -231.659)" fill="#fff"/>
-                                <path id="Tracé_272" data-name="Tracé 272" d="M262.111,241.508a.869.869,0,0,1-.552-.216l-.093-.112a.552.552,0,0,1-.07-.134.459.459,0,0,1-.047-.134,1.068,1.068,0,0,1-.016-.149.764.764,0,0,1,.225-.528.829.829,0,0,1,1.1,0,.764.764,0,0,1,.225.528,1.072,1.072,0,0,1-.015.149.459.459,0,0,1-.047.134.553.553,0,0,1-.07.134,1.182,1.182,0,0,1-.093.112A.837.837,0,0,1,262.111,241.508Z" transform="translate(-251.812 -231.659)" fill="#fff"/>
-                                <path id="Tracé_273" data-name="Tracé 273" d="M112.777,316.412a.8.8,0,0,1-.3-.059.9.9,0,0,1-.257-.156.764.764,0,0,1-.225-.528.713.713,0,0,1,.062-.283.691.691,0,0,1,.163-.245.842.842,0,0,1,1.1,0,.731.731,0,0,1,0,1.055.837.837,0,0,1-.552.216" transform="translate(-107.919 -303.96)" fill="#fff"/>
-                                <path id="Tracé_274" data-name="Tracé 274" d="M187.444,316.412a.837.837,0,0,1-.552-.216.764.764,0,0,1-.225-.528.713.713,0,0,1,.062-.283.691.691,0,0,1,.163-.245.842.842,0,0,1,1.1,0,.691.691,0,0,1,.163.245.713.713,0,0,1,.062.283.764.764,0,0,1-.225.528.837.837,0,0,1-.552.216" transform="translate(-179.865 -303.96)" fill="#fff"/>
-                                <path id="Tracé_275" data-name="Tracé 275" d="M262.111,316.134a.837.837,0,0,1-.552-.216.691.691,0,0,1-.163-.245.673.673,0,0,1,0-.565.691.691,0,0,1,.163-.245.8.8,0,0,1,.7-.2.484.484,0,0,1,.148.045.6.6,0,0,1,.14.067,1.215,1.215,0,0,1,.117.089.731.731,0,0,1,0,1.056A.837.837,0,0,1,262.111,316.134Z" transform="translate(-251.812 -303.683)" fill="#fff"/>
-                                <path id="Tracé_276" data-name="Tracé 276" d="M24.464,152.365H11.249a.558.558,0,1,1,0-1.115H24.464a.558.558,0,1,1,0,1.115" transform="translate(-10.277 -145.98)" fill="#fff"/>
-                                <path id="Tracé_277" data-name="Tracé 277" d="M10.688,46.867H4.47C1.632,46.867,0,45.306,0,42.593V36.274C0,33.561,1.632,32,4.47,32h6.219c2.837,0,4.47,1.561,4.47,4.274v6.319c0,2.713-1.632,4.274-4.47,4.274M4.47,33.115c-2.223,0-3.3,1.033-3.3,3.159v6.319c0,2.126,1.081,3.159,3.3,3.159h6.219c2.223,0,3.3-1.033,3.3-3.159V36.274c0-2.126-1.08-3.159-3.3-3.159Z" transform="translate(0 -30.885)" fill="#fff"/>
-                            </g>
-                        </svg> -->
                         <span data-target-tpl="button-label"></span>
                     </span>
                 </div>
@@ -212,6 +155,35 @@
     </div>
 </template>
 
+<template id="coopleo-result-simplified-tpl">
+    <a href="#" class="result-card-simplified" <?php echo ($vars['openNewTab'] ? 'target="_blank"': '') ?> data-target-tpl="button-link">
+        <div class="result-card-therapist-infos">
+            <img src="<?php echo COOPLEO_PLUGIN_URL . 'assets/icons/therapist-no-picture.png'; ?>" alt="Photo de profil">
+            <div>
+                <h2 class="result-card-therapist-name" data-target-tpl="name"></h2>
+                <h3 class="result-card-therapist-type" data-target-tpl="type"></h3>
+                <div class="result-card-therapist-detail result-card-therapist-address">
+                    <div class="svg-container">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-map-pin-icon lucide-map-pin"><path d="M20 10c0 4.993-5.539 10.193-7.399 11.799a1 1 0 0 1-1.202 0C9.539 20.193 4 14.993 4 10a8 8 0 0 1 16 0"/><circle cx="12" cy="10" r="3"/></svg>
+                    </div>
+                    <p data-target-tpl="address"></p>
+                </div>
+            </div>
+        </div>
+        <div class="result-card-therapist-rdv">
+            <div class="rdv-type-list">
+                <div class="rdv-type-cabinet">
+                    <img src="<?php echo COOPLEO_PLUGIN_URL . 'assets/icons/visio-en-cabinet-rond.png'; ?>" alt="">
+                    <p class="tooltip">En cabinet</p>
+                </div>
+            </div>
+            <span class="coopleo-button">
+                Contacter
+            </span>
+        </div>
+    </a>
+</template>
+
 <script type="module">
     import {DateTime} from 'https://cdn.jsdelivr.net/npm/luxon@3.6.1/+esm';
 
@@ -219,6 +191,7 @@
     const searchURL = "<?php echo COOPLEO_API_ENDPOINT; ?>";
     const only_fsf = !!("<?php echo $vars['fsf_only'] ?>" === "1");
     const resultTpl = document.querySelector("#coopleo-result-tpl");
+    const resultSimplifiedTpl = document.querySelector("#coopleo-result-simplified-tpl");
     const resultDayTpl = document.querySelector("#coopleo-result-day-tpl");
     const resultsContainer = document.querySelector("#coopleo-results-container");
     const nextPageButton = document.querySelector("#coopleo-results-pagination-next");
@@ -366,7 +339,11 @@
         resultsContainer.innerHTML = "";
         if (results.status === "success" && results.data.length) {
             results.data.forEach(result => {
-                generateResult(result.document);
+                if (result.document.no_adhesion) {
+                    generateSimplifiedResult(result.document);
+                }else{
+                    generateResult(result.document);
+                }
             });
         }else{
             noResultsContainer.style.display = "block";
@@ -395,6 +372,28 @@
         }
         replaceTplContent(main, "results-count", resultCountSentence);
 
+    }
+
+    function generateSimplifiedResult(data){
+        const result = resultSimplifiedTpl.content.cloneNode(true);
+        replaceTplContent(result, "name", data.name);
+        if (data.photo_link) {
+            result.querySelector(".result-card-therapist-infos img").src = data.photo_link;
+        }
+        if (filters.q && data.type_therapist.includes(filters.q)) {
+            replaceTplContent(result, "type", data.type_therapist.find((type) => type.toLowerCase() === filters.q.toLowerCase()) ?? '');
+        }else{
+            replaceTplContent(result, "type", data.type_therapist[0] ?? '');
+        }
+        if (data.localisation.adresse) {
+            replaceTplContent(result, "address", data.localisation.adresse.split(",").find(el => el.match(/\d{5}/)) ?? data.localisation.adresse);
+        }else{
+            deleteTplElement(result, ".result-card-therapist-address");
+        }
+        if(data.link){
+            result.querySelector("[data-target-tpl='button-link']").href = data.link;
+        }
+        resultsContainer.appendChild(result);
     }
 
     function generateResult(data){
