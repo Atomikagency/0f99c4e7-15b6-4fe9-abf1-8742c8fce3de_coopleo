1ee5c50da2b361348880f0e7c3056d0db0139435
Kevin JANIKY <kevin.janiky@gmail.com>
2025-05-16 13:32:34 +0200

    Remove webhook conditionnal

--- src/includes/welcome-email-on-register.php
+++ src/includes/welcome-email-on-register.php
@@ -5,10 +5,6 @@ add_action('user_register', 'envoyer_mail_si_pas_amelia_employee', 10, 1);
 
 function envoyer_mail_si_pas_amelia_employee($user_id)
 {
-    $settings = get_option('coopleo_search_engine_options');
-    if (!empty($settings['active_webhook_new_account'])) {
-
-
         $user = get_userdata($user_id);
         $webhook_url = 'https://hook.eu2.make.com/lc3x65m5jmf48u2ggjcbkpwztu1rsub7';
         $reset_key = get_password_reset_key($user);
@@ -39,8 +35,6 @@ function envoyer_mail_si_pas_amelia_employee($user_id)
         if (curl_errno($ch)) {
             error_log('Erreur cURL : ' . curl_error($ch));
         }
-
         curl_close($ch);
-    }
 
 }
