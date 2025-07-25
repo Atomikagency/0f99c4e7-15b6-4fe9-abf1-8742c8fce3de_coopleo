53d52584fc2af4610a1ce144d988ebe99f172c3b
Clement Menczynski <71343161+clementmenc@users.noreply.github.com>
2025-07-25 11:55:01 +0200

    Small fix

--- src/templates/menu.tpl.php
+++ src/templates/menu.tpl.php
@@ -157,10 +157,10 @@ function coopleo_render_submenu($items_by_parent, $parent_id, $depth = 1) {
                                 </div>
                                 <div class="problematique-dropdown-cta">
                                     <p class="cta-title">Prenez soin de votre couple</p>
-                                    <button class="find-pro">
+                                    <a href="/trouver-therapeute-de-couple" class="find-pro">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-search-icon lucide-search"><path d="m21 21-4.34-4.34"/><circle cx="11" cy="11" r="8"/></svg>    
                                         Trouver un professionnel
-                                    </button>
+                                    </a>
                                     <div>
                                         <p class="question-title">Vous avez des questions ?</p>
                                         <p><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle-question-mark-icon lucide-circle-question-mark"><circle cx="12" cy="12" r="10"/><path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3"/><path d="M12 17h.01"/></svg>FAQ Complète</p>
@@ -183,10 +183,10 @@ function coopleo_render_submenu($items_by_parent, $parent_id, $depth = 1) {
     </nav>
 
     <div class="coopleo-actions">
-        <button class="find-pro">
+        <a href="/trouver-therapeute-de-couple" class="find-pro">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-search-icon lucide-search"><path d="m21 21-4.34-4.34"/><circle cx="11" cy="11" r="8"/></svg>    
             <span>Trouver un professionnel</span>
-        </button>
+        </a>
         <button class="connection">
             <i aria-hidden="true" class="material-icons md-person_outline"></i>
         </button>
