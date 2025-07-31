80a301e3afd48c4a2be628ac9d20d888402872e4
Clement Menczynski <71343161+clementmenc@users.noreply.github.com>
2025-07-31 11:13:07 +0200

    Update coopleo.php

--- src/coopleo.php
+++ src/coopleo.php
@@ -36,7 +36,7 @@ function ta_enqueue_styles()
     wp_enqueue_style('coopleo-search', COOPLEO_PLUGIN_URL . 'assets/css/search.css', [], '1.9.4');
     wp_enqueue_style('coopleo-results', COOPLEO_PLUGIN_URL . 'assets/css/results.css', [], '1.9.5');
     wp_enqueue_style('coopleo-dual-range', COOPLEO_PLUGIN_URL . 'assets/css/dual-range.css', [], '1.9.4');
-    wp_enqueue_style('coopleo-menu', COOPLEO_PLUGIN_URL . 'assets/css/menu.css', [], '1.1.0');
+    wp_enqueue_style('coopleo-menu', COOPLEO_PLUGIN_URL . 'assets/css/menu.css', [], '1.2.0');
     wp_enqueue_script_module('coopleo-dual-range-input', COOPLEO_PLUGIN_URL . 'assets/js/dual-range-input.js', [], '1.9.4');
     wp_enqueue_script_module('coopleo-menu', COOPLEO_PLUGIN_URL . 'assets/js/menu.js', [], '1.1.0');
 }
